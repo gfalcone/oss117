36fcaab5f02dcded483f2336654a305977ee5a38
Paolo Genissel-Monsallier <paolo.genissel-monsallier@1000mercis.com>
2018-07-23 16:24:26 +0200

    Modified token

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def main():
     resp_data = {}
     resp_data['text'] = "<https://youtu.be/xGKowUNYrKA>"
-    resp_data['token']= "9d696dkhbpykjm6ee6rarex8qw"
+    resp_data['token']= "5pwjbt39zj8i5dtux7jkaci4wc"
     
     resp = Response(content_type='application/json')
     resp.set_data(json.dumps(resp_data))
